d166489fe94d8e843b840cc9e64bad8e0f5286f5
Dzevad <dzevad.del@gmail.com>
2022-04-05 02:33:54 +0200

    finished mobile design

--- components/adviceGeneretor/adviceGenerator.tsx
+++ components/adviceGeneretor/adviceGenerator.tsx
@@ -1,52 +1,65 @@
 import axios from 'axios';
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer, useState } from 'react';
 
 import styles from './adviceGenerator.module.scss';
 
+interface IAdvice {
+  advice?: string;
+  id?: string;
+}
+
 const AdviceGenerator = () => {
-  const [advice, setAdvice] = useState('');
-  const [id, setId] = useState('');
+  const [advice, setAdvice] = useState<IAdvice>({});
+  const [defaultAdvice] = useState({
+    advice:
+      "It is easy to sit up and take notice, what's difficult is getting up and taking action.",
+    id: '117',
+  });
 
   const handleGenerateAdvice = async () => {
     try {
       const { data } = await axios.get('https://api.adviceslip.com/advice');
       const { slip } = data;
 
-      setAdvice(slip.advice);
-      setId(slip.id);
+      setAdvice(slip);
     } catch (err) {
       console.error(err);
     }
   };
 
-  useEffect(() => {
-    handleGenerateAdvice();
-  }, []);
-
-  console.log('advice >>> ', advice);
-  console.log('id >>> ', id);
   return (
     <article className={styles.advice}>
-      <h2 className={styles.title}>Advice &#x00023;{id}</h2>
+      <h2 className={styles.title}>
+        Advice &#35;{advice.id ? advice.id : defaultAdvice.id}
+      </h2>
       <p className={styles.text}>
-        {/* {advice} */}
-        &ldquo;It is easy to sit up and take notice, what&lsquo;s difficult is
-        getting up and taking action.&ldquo;
+        &ldquo;{advice.advice ? advice.advice : defaultAdvice.advice}
+        &rdquo;
       </p>
       <div className={styles.graphic}>
-        <Image
-          src='/images/pattern-divider-mobile.svg'
-          alt='two lines divder'
-          width={295}
-          height={16}
-        />
+        <div className={styles.mobileSvg}>
+          <Image
+            src='/images/pattern-divider-mobile.svg'
+            alt='two vertical lines divder'
+            width={295}
+            height={16}
+          />
+        </div>
+        <div className={styles.desktopSvg}>
+          <Image
+            src='/images/pattern-divider-desktop.svg'
+            alt='two vertical lines divder'
+            width={444}
+            height={16}
+          />
+        </div>
       </div>
-      <button className={styles.button}>
+      <button className={styles.button} onClick={handleGenerateAdvice}>
         <div className={styles.iconDiceWrapper}>
           <Image
             src='/images/icon-dice.svg'
-            alt='dice cuber number 5'
+            alt='dice cube number 5'
             width={24}
             height={24}
           />
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import AdviceGenerator from '../components/adviceGeneretor/adviceGenerator';
-import styles from '../styles/Home.module.css';
+import styles from '../styles/pages/Home.module.scss';
 
 const Home: NextPage = () => {
   return (
